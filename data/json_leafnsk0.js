28a12e5cc5070d12fd82d902e685517bf7b80e4d
Alexey Kolesnikow <alekseykw@gmail.com>
2017-10-06 17:53:30 +0700

    first

---
+++ data/json_leafnsk0.js
@@ -0,0 +1,8 @@
+var json_leafnsk0={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "id": 1.000000, "labels": "SSUGT" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 82.862990941360934, 54.987122229173977 ], [ 82.863120939727523, 54.986121168126537 ], [ 82.862758312704941, 54.986093687667264 ], [ 82.862628314338366, 54.987106526448329 ], [ 82.862990941360934, 54.987122229173977 ] ] ] } }
+]
+}
